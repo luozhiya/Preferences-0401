b610766068060007ab1729cc17a22d4e750c64a5
luozhiya <luozhiya@petalmail.com>
2023-04-23 22:24:38 +0800

    https://github.com/luukvbaal/statuscol.nvim/issues/58

---
+++ any/vi-family/nvim/minimal.lua
@@ -0,0 +1,21 @@
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({ "git", "clone", "--depth=1", "https://github.com/savq/paq-nvim.git", install_path })
+end
+
+require("paq")({
+  "savq/paq-nvim",
+  "luukvbaal/statuscol.nvim",
+  "b0o/incline.nvim"
+})
+
+vim.opt.foldcolumn = '1'
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
+
+require('statuscol').setup()
+-- require('incline').setup()
+
